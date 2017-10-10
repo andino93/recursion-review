c1e00d0df9a28b563a6a712e1baff74908dbbddc
andino93 <andy.andino93@gmail.com>
2017-10-10 11:14:56 -0700

    added code for getElements and stringify

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var array = [];
+  var searchNodes = function(node) {
+    if (node.classList.contains(className)) {
+      array.push(node);
+    } 
+    Array.prototype.forEach.call(node.children, searchNodes);
+  };
+  searchNodes(document.body);
+  return array;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,17 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj)) {
+    return '[' + obj.map(stringifyJSON) + ']';
+  }
+  if (typeof obj === 'object' && obj) {
+    var keys = Object.keys(obj);
+    
+    keys = keys.filter(key => typeof obj[key] !== 'function' && obj[key] !== undefined);
+    //key => stringifyJSON(key) + ':' + stringifyJSON(obj[key])
+    return '{' + keys.map(function(key) {
+      return stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+    }) + '}';
+  }
+  return typeof obj === 'string' ? '"' + obj + '"' : String(obj);
 };
